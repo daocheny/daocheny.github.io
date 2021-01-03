f797f8df44afa2147f1ec09e9370499edd1b83c4
Dao Chen Yuan <dao.yuan@mail.mcgill.ca>
2021-01-03 02:47:28 +0000

    Added aesthetic elements

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let body;
 let darkmode;
 let rotate = 0;
+let cherry;
+let night;
 function handleLoad () {
   body = document.getElementsByTagName("BODY")[0];
   darkmode = document.getElementById("darkmode");
   darkmode.addEventListener("click", switchMode);
-  
+  cherry = document.querySelectorAll(".cherry");
+  night = document.querySelectorAll(".night");
+
   const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
   if ((document.cookie === "" && prefersDarkScheme.matches) || document.cookie === "t") {
+    switchMode();
+  } else {
+    switchMode();
     switchMode();
   }
 }
@@ -18,9 +25,13 @@ function switchMode () {
   if (body.classList.toggle("darkmode")) {
     darkmode.title = "Click me to turn off dark mode!";
     document.cookie = "t";
+    cherry.forEach(x => x.hidden = true);
+    night.forEach(x => x.hidden = false);
   } else {
     darkmode.title = "Click me to turn on dark mode!";
     document.cookie = "f";
+    cherry.forEach(x => x.hidden = false);
+    night.forEach(x => x.hidden = true);
   }
 }
 window.addEventListener("load", handleLoad);
